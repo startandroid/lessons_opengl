d9073977f22f11d7df8f5d653cc9c6329263154c
Dmitry <startofandroid@gmail.com>
2015-12-07 22:51:09 +0300

    lesson 172, perspective

---
+++ lesson172_perspective/src/main/java/ru/startandroid/l172perspective/FileUtils.java
@@ -0,0 +1,37 @@
+package ru.startandroid.l172perspective;
+import android.content.Context;
+import android.content.res.Resources;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class FileUtils {
+
+    public static String readTextFromRaw(Context context, int resourceId) {
+        StringBuilder stringBuilder = new StringBuilder();
+        try {
+            BufferedReader bufferedReader = null;
+            try {
+                InputStream inputStream =
+                        context.getResources().openRawResource(resourceId);
+                bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                String line;
+                while ((line = bufferedReader.readLine()) != null) {
+                    stringBuilder.append(line);
+                    stringBuilder.append("\r\n");
+                }
+            } finally {
+                if (bufferedReader != null) {
+                    bufferedReader.close();
+                }
+            }
+        } catch (IOException ioex) {
+            ioex.printStackTrace();
+        } catch (Resources.NotFoundException nfex) {
+            nfex.printStackTrace();
+        }
+        return stringBuilder.toString();
+    }
+}
---
+++ lesson172_perspective/src/main/java/ru/startandroid/l172perspective/OpenGLRenderer.java
@@ -0,0 +1,98 @@
+package ru.startandroid.l172perspective;
+
+import android.content.Context;
+import android.opengl.GLSurfaceView.Renderer;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import static android.opengl.GLES20.GL_COLOR_BUFFER_BIT;
+import static android.opengl.GLES20.GL_FLOAT;
+import static android.opengl.GLES20.GL_FRAGMENT_SHADER;
+import static android.opengl.GLES20.GL_POINTS;
+import static android.opengl.GLES20.GL_VERTEX_SHADER;
+import static android.opengl.GLES20.glClear;
+import static android.opengl.GLES20.glClearColor;
+import static android.opengl.GLES20.glDrawArrays;
+import static android.opengl.GLES20.glEnableVertexAttribArray;
+import static android.opengl.GLES20.glGetAttribLocation;
+import static android.opengl.GLES20.glGetUniformLocation;
+import static android.opengl.GLES20.glUniform4f;
+import static android.opengl.GLES20.glUseProgram;
+import static android.opengl.GLES20.glVertexAttribPointer;
+import static android.opengl.GLES20.glViewport;
+
+public class OpenGLRenderer implements Renderer {
+
+    private final static int POSITION_COUNT = 4;
+
+    private Context context;
+
+    private int programId;
+
+    private FloatBuffer vertexData;
+    private int uColorLocation;
+    private int aPositionLocation;
+    private int uMatrixLocation;
+
+    private float[] mProjectionMatrix = new float[16];
+
+    public OpenGLRenderer(Context context) {
+        this.context = context;
+        prepareData();
+    }
+
+    @Override
+    public void onSurfaceCreated(GL10 arg0, EGLConfig arg1) {
+        glClearColor(0f, 0f, 0f, 1f);
+        int vertexShaderId = ShaderUtils.createShader(context, GL_VERTEX_SHADER, R.raw.vertex_shader);
+        int fragmentShaderId = ShaderUtils.createShader(context, GL_FRAGMENT_SHADER, R.raw.fragment_shader);
+        programId = ShaderUtils.createProgram(vertexShaderId, fragmentShaderId);
+        glUseProgram(programId);
+        bindData();
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 arg0, int width, int height) {
+        glViewport(0, 0, width, height);
+    }
+
+    private void prepareData() {
+        float x1 = -0.5f, y1 = -0.8f, x2 = 0.5f, y2 = -0.8f;
+
+        float[] vertices = {
+                x1, y1, 0.0f, 1.0f,
+                x2, y2, 0.0f, 1.0f,
+        };
+
+        vertexData = ByteBuffer
+                .allocateDirect(vertices.length * 4)
+                .order(ByteOrder.nativeOrder())
+                .asFloatBuffer();
+        vertexData.put(vertices);
+    }
+
+    private void bindData() {
+        // координаты
+        aPositionLocation = glGetAttribLocation(programId, "a_Position");
+        vertexData.position(0);
+        glVertexAttribPointer(aPositionLocation, POSITION_COUNT, GL_FLOAT,
+                false, 0, vertexData);
+        glEnableVertexAttribArray(aPositionLocation);
+
+        // цвет
+        uColorLocation = glGetUniformLocation(programId, "u_Color");
+    }
+
+    @Override
+    public void onDrawFrame(GL10 arg0) {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glUniform4f(uColorLocation, 0.0f, 1.0f, 0.0f, 1.0f);
+        glDrawArrays(GL_POINTS, 0, 2);
+    }
+
+}
