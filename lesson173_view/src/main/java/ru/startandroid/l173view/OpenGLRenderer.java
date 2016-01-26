42a8905dfd7f32d564b103ae031a8e4113007199
dmitry <startofandroid@gmail.com>
2016-01-26 22:24:31 +0300

    lesson 173 view

---
+++ lesson173_view/src/main/java/ru/startandroid/l173view/OpenGLRenderer.java
@@ -0,0 +1,215 @@
+package ru.startandroid.l173view;
+
+import android.content.Context;
+import android.opengl.GLSurfaceView.Renderer;
+import android.opengl.Matrix;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import static android.opengl.GLES20.GL_COLOR_BUFFER_BIT;
+import static android.opengl.GLES20.GL_FLOAT;
+import static android.opengl.GLES20.GL_FRAGMENT_SHADER;
+import static android.opengl.GLES20.GL_LINES;
+import static android.opengl.GLES20.GL_VERTEX_SHADER;
+import static android.opengl.GLES20.glClear;
+import static android.opengl.GLES20.glClearColor;
+import static android.opengl.GLES20.glDrawArrays;
+import static android.opengl.GLES20.glEnableVertexAttribArray;
+import static android.opengl.GLES20.glGetAttribLocation;
+import static android.opengl.GLES20.glGetUniformLocation;
+import static android.opengl.GLES20.glUniform4f;
+import static android.opengl.GLES20.glUseProgram;
+import static android.opengl.GLES20.glVertexAttribPointer;
+import static android.opengl.GLES20.glViewport;
+import static android.opengl.GLES20.glUniformMatrix4fv;
+import static android.opengl.GLES20.GL_TRIANGLES;
+import static android.opengl.GLES20.GL_DEPTH_BUFFER_BIT;
+import static android.opengl.GLES20.GL_DEPTH_TEST;
+import static android.opengl.GLES20.glEnable;
+import static android.opengl.GLES20.glLineWidth;
+
+public class OpenGLRenderer implements Renderer {
+
+    private final static int POSITION_COUNT = 3;
+
+    private Context context;
+
+    private FloatBuffer vertexData;
+    private int uColorLocation;
+    private int aPositionLocation;
+    private int uMatrixLocation;
+    private int programId;
+
+    private float[] mProjectionMatrix = new float[16];
+    private float[] mViewMatrix = new float[16];
+    private float[] mMatrix = new float[16];
+
+    public OpenGLRenderer(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public void onSurfaceCreated(GL10 arg0, EGLConfig arg1) {
+        glClearColor(0f, 0f, 0f, 1f);
+        glEnable(GL_DEPTH_TEST);
+        int vertexShaderId = ShaderUtils.createShader(context, GL_VERTEX_SHADER, R.raw.vertex_shader);
+        int fragmentShaderId = ShaderUtils.createShader(context, GL_FRAGMENT_SHADER, R.raw.fragment_shader);
+        programId = ShaderUtils.createProgram(vertexShaderId, fragmentShaderId);
+        glUseProgram(programId);
+        createViewMatrix();
+        prepareData();
+        bindData();
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 arg0, int width, int height) {
+        glViewport(0, 0, width, height);
+        createProjectionMatrix(width, height);
+        bindMatrix();
+    }
+
+    private void prepareData() {
+
+        float s = 0.4f;
+        float d = 0.9f;
+        float l = 3;
+
+        float[] vertices = {
+
+                // первый треугольник
+                -2*s, -s, d,
+                2*s, -s, d,
+                0, s, d,
+
+                // второй треугольник
+                -2*s, -s, -d,
+                2*s, -s, -d,
+                0, s, -d,
+
+                // третий треугольник
+                d, -s, -2*s,
+                d, -s, 2*s,
+                d, s, 0,
+
+                // четвертый треугольник
+                -d, -s, -2*s,
+                -d, -s, 2*s,
+                -d, s, 0,
+
+                // ось X
+                -l, 0,0,
+                l,0,0,
+
+                // ось Y
+                0,-l,0,
+                0,l,0,
+
+                // ось Z
+                0,0,-l,
+                0,0,l,
+        };
+
+        vertexData = ByteBuffer
+                .allocateDirect(vertices.length * 4)
+                .order(ByteOrder.nativeOrder())
+                .asFloatBuffer();
+        vertexData.put(vertices);
+    }
+
+    private void bindData() {
+        // примитивы
+        aPositionLocation = glGetAttribLocation(programId, "a_Position");
+        vertexData.position(0);
+        glVertexAttribPointer(aPositionLocation, POSITION_COUNT, GL_FLOAT,
+                false, 0, vertexData);
+        glEnableVertexAttribArray(aPositionLocation);
+
+        // цвет
+        uColorLocation = glGetUniformLocation(programId, "u_Color");
+
+        // матрица
+        uMatrixLocation = glGetUniformLocation(programId, "u_Matrix");
+    }
+
+    private void createProjectionMatrix(int width, int height) {
+        float ratio = 1;
+        float left = -1;
+        float right = 1;
+        float bottom = -1;
+        float top = 1;
+        float near = 2;
+        float far = 8;
+        if (width > height) {
+            ratio = (float) width / height;
+            left *= ratio;
+            right *= ratio;
+        } else {
+            ratio = (float) height / width;
+            bottom *= ratio;
+            top *= ratio;
+        }
+
+        Matrix.frustumM(mProjectionMatrix, 0, left, right, bottom, top, near, far);
+    }
+
+    private void createViewMatrix() {
+        // точка положения камеры
+        float eyeX = 0;
+        float eyeY = 0;
+        float eyeZ = 3;
+
+        // точка направления камеры
+        float centerX = 0;
+        float centerY = 0;
+        float centerZ = 0;
+
+        // up-вектор
+        float upX = 0;
+        float upY = 1;
+        float upZ = 0;
+
+        Matrix.setLookAtM(mViewMatrix, 0, eyeX, eyeY, eyeZ, centerX, centerY, centerZ, upX, upY, upZ);
+    }
+
+
+    private void bindMatrix() {
+        Matrix.multiplyMM(mMatrix, 0, mProjectionMatrix, 0, mViewMatrix, 0);
+        glUniformMatrix4fv(uMatrixLocation, 1, false, mMatrix, 0);
+    }
+
+    @Override
+    public void onDrawFrame(GL10 arg0) {
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // треугольники
+        glUniform4f(uColorLocation, 0.0f, 1.0f, 0.0f, 1.0f);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        glUniform4f(uColorLocation, 0.0f, 0.0f, 1.0f, 1.0f);
+        glDrawArrays(GL_TRIANGLES, 3, 3);
+
+        glUniform4f(uColorLocation, 1.0f, 0.0f, 0.0f, 1.0f);
+        glDrawArrays(GL_TRIANGLES, 6, 3);
+
+        glUniform4f(uColorLocation, 1.0f, 1.0f, 0.0f, 1.0f);
+        glDrawArrays(GL_TRIANGLES, 9, 3);
+
+        // оси
+        glLineWidth(1);
+
+        glUniform4f(uColorLocation, 0.0f, 1.0f, 1.0f, 1.0f);
+        glDrawArrays(GL_LINES, 12, 2);
+
+        glUniform4f(uColorLocation, 1.0f, 0.0f, 1.0f, 1.0f);
+        glDrawArrays(GL_LINES, 14, 2);
+
+        glUniform4f(uColorLocation, 1.0f, 0.5f, 0.0f, 1.0f);
+        glDrawArrays(GL_LINES, 16, 2);
+    }
+
+}
---
+++ lesson173_view/src/main/res/raw/vertex_shader.glsl
@@ -0,0 +1,8 @@
+attribute vec4 a_Position;
+uniform mat4 u_Matrix;
+ 
+void main()
+{
+    gl_Position = u_Matrix * a_Position;
+    gl_PointSize = 5.0;
+}
